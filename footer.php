65919610b4f67e9860f000a85444c534a49852cd
Augusto <Augusto@192.168.100.226>
2015-06-10 12:21:25 -0600

    09062015

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="clear"></div>
+</div>
+<footer id="footer" role="contentinfo">
+<div id="copyright">
+<?php echo sprintf( __( '%1$s %2$s %3$s. Tddos los derechos reservados.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo ' Tema por:'.'<a href="http://aemcode.com/">@emcode</a>' ; ?>
+</div>
+</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
